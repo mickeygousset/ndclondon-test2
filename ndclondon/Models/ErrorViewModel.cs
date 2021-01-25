1599ad4f7265a94c47abaad175342d6f460006d7
Mickey Gousset <mickeygousset@github.com>
2021-01-25 07:48:09 -0600

    Initial Version

---
+++ ndclondon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ndclondon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
